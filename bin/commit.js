4003cc4e0d2f0052d6874e42fab695ac3c5e96a7
Jacek Berbecki <berbecki@gmail.com>
2019-03-21 23:37:00 +0100

    add zipper

--- bin/commit.js
+++ bin/commit.js
@@ -1,4 +1,28 @@
 #!/usr/bin/env node
 
-console.log('abc')
+const fs = require("fs");
+const path = require("path");
+const unzipper = require("unzipper");
 
+const sourceFolder = "./source";
+const trackedFolder = "./tracked";
+const filenames = [];
+
+fs.readdirSync(sourceFolder).forEach(file => {
+  if(file.split(".")[1] === 'sketch') {
+    filenames.push({ file, name: file.split(".")[0] });
+  }
+});
+
+filenames.forEach(({ name, file }) => {
+  fs.mkdirSync(`${trackedFolder}/${name}`, { recursive: true });
+  fs.copyFileSync(
+    path.resolve(`${sourceFolder}/${file}`),
+    path.resolve(`${sourceFolder}/${name}.zip`)
+  );
+  console.log("path:");
+  fs.createReadStream(path.resolve(`${sourceFolder}/${name}.zip`))
+    .pipe(unzipper.Extract({ path: path.resolve(`${trackedFolder}/${name}`) }));
+});
+
+console.log("farr", filenames);
